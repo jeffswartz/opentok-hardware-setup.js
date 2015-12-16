e941876dcddc0a9bc56b8c5e1c25a64c13788931
Jeff Swartz <swartz@tokbox.com>
2015-12-16 11:57:30 -0800

    adding express server

--- tests/webdriver/tests.js
+++ tests/webdriver/tests.js
@@ -1,17 +1,24 @@
-/*
-* How many /wiki links are on the /wiki page.
-*/
-
 "use strict";
 
+var express = require('express');
+// var assert = require('assert');
+// var test = require('selenium-webdriver/testing');
 var webdriver = require('selenium-webdriver');
+
+// Set up test server:
+var app = express();
+
+app.use(express.static('.'));
+var server = app.listen(3030);
+
+// Start integration tests:
 var browser = new webdriver.Builder().usingServer().withCapabilities({'browserName': 'firefox' }).build();
 
-browser.get('https://2c53be92.ngrok.com/hardware-setup/');
+browser.get('http://localhost:3030/index.html');
 
-console.log('start');
 browser.findElements(webdriver.By.className('opentok-hardware-setup')).then(function(elements){
-	console.log('Found', elements.length, 'opentok-hardware-setup' )
+	console.log('Found', elements.length, 'opentok-hardware-setup' );
+	//assertEquals('a', ['a'][0]);
 });
 
 browser.wait(function() {
@@ -23,15 +30,18 @@ browser.findElements(webdriver.By.className('opentok-hardware-setup-camera')).th
 browser.findElements(webdriver.By.className('opentok-hardware-setup-mic')).then(function(elements){
 	console.log('Found', elements.length, 'opentok-hardware-setup-mic' )
 });
+browser.findElements(webdriver.By.className('opentok-hardware-setup-selector')).then(function(elements) {
+	console.log('Found', elements.length, 'opentok-hardware-setup-selector' );
+	elements[0].click();
+});
 browser.quit();
 
-/***
-
+/*
 test.describe('Google Search', function() {
   var driver;
 
   test.before(function() {
-    driver = new firefox.Driver();
+    driver = new webdriver.Builder().usingServer().withCapabilities({'browserName': 'firefox' }).build();
   });
 
   test.after(function() {
@@ -40,8 +50,8 @@ test.describe('Google Search', function() {
 
   test.it('should append query to title', function() {
     driver.get('http://www.google.com/ncr');
-    driver.findElement(By.name('q')).sendKeys('webdriver');
-    driver.findElement(By.name('btnG')).click();
+    driver.findElements(webdriver.By.name('q')).click().sendKeys('webdriver');
+    driver.findElements(webdriver.By.name('btnG')).click();
     driver.wait(until.titleIs('webdriver - Google Search'), 1000);
   });
 });
